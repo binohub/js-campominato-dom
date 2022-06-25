d9bf003f94eee78ee3eb4514005cc828022f0f48
binohub <yeebino001@gmail.com>
2022-06-25 04:29:31 +0200

    ms2 - number & class dynamic (difficulty) - finish

--- js/main.js
+++ js/main.js
@@ -1,65 +1,46 @@
-// ! al click di hard avvia square hard
-const difficultyHard = document.getElementById(`difficulty-hard`);
-difficultyHard.addEventListener('click', function(){
-    const squareContainer = document.getElementById("square-container")
-    squareContainer.textContent ="";
+document.querySelector("#btn-reset").addEventListener("click", function () {
+    functionCreateNewGame();
+});
+
+
+// ### functions ### functions ### functions ### functions ### functions
+function functionCreateNewGame() {
+    const squareContainer = document.querySelector("#square-container");
+    squareContainer.innerHTML = "";
+
+    // ! number cycle & class square dynamic
+    let n = 0; 
+    let c = "";
+    if (document.querySelector("#difficulty-hard").selected === true) {
+        n = 100;
+        c = "square-hard";
+    }else if(document.querySelector("#difficulty-medium").selected === true) {
+        n = 81;
+        c = "square-medium";
+    }else{
+        n = 49;
+        c = "square-easy";
+    }
+
     // ! crea un quadrato * 100 e assegna al quadrato il suo numero
-    for (let i = 1; i <= 100; i++){
+    for (let i = 1; i <= n; i++) {
         // ! crea un div con classe square da aggiungere
         const newDiv = document.createElement('div');
-        newDiv.classList.add('square-hard');
+        newDiv.classList.add(c);
         newDiv.innerHTML = i;
-        // // ! aggiungi la classe container-hard 
-        // squareContainer.classList.add('container-hard');
-
         // ! al click fai pari o dispari? a seconda del risultato aggiungi una classe
-        newDiv.addEventListener('click', function(){
-        if (i % 2 === 0){
-            newDiv.classList.toggle("bg-success");
-            console.log(`hai cliccato la casella numero ${i}`);
-        }else{
-            newDiv.classList.toggle("bg-danger");
-            console.log(`hai cliccato la casella numero ${i}`);
+        newDiv.addEventListener('click', function () {
+            if (i % 2 === 0) {
+                newDiv.classList.toggle("bg-success");
+                console.log(`hai cliccato la casella numero ${i}`);
+            } else {
+                newDiv.classList.toggle("bg-danger");
+                console.log(`hai cliccato la casella numero ${i}`);
+            }
         }
-        })
+        )
         // ! conneecting
         squareContainer.append(newDiv)
-        squareContainer.classList.remove("d-none");
+        // squareContainer.classList.remove("d-none");
     }
-})
-// // ! al click di MEDIUM avvia square hard
-// const difficultyMedium = document.getElementById(`difficulty-medium`);
-// difficultyHard.addEventListener('click', function(){
-//     const squareContainer = document.getElementById("square-container")
-//     squareContainer.textContent ="";
-//     // ! crea un quadrato * 100 e assegna al quadrato il suo numero
-//     for (let i = 1; i <= 100; i++){
-//         // ! crea un div con classe square da aggiungere
-//         const newDiv = document.createElement('div');
-//         newDiv.classList.add('square-medium');
-//         newDiv.innerHTML = i;
-//         // // ! aggiungi la classe container-hard 
-//         // squareContainer.classList.add('container-hard');
-
-//         // ! al click fai pari o dispari? a seconda del risultato aggiungi una classe
-//         newDiv.addEventListener('click', function(){
-//         if (i % 2 === 0){
-//             newDiv.classList.toggle("bg-success");
-//             console.log(`hai cliccato la casella numero ${i}`);
-//         }else{
-//             newDiv.classList.toggle("bg-danger");
-//             console.log(`hai cliccato la casella numero ${i}`);
-//         }
-//         })
-//         // ! conneecting
-//         squareContainer.append(newDiv)
-//         squareContainer.classList.remove("d-none");
-//     }
-// })
-// ! al click di reset aggiungi d-none
-const btnReset = document.querySelector("#btn-reset");
-btnReset.addEventListener("click", function(){
-    const squareContainer = document.querySelector("#square-container");    
-    squareContainer.textContent ="";
-})
-
+}
